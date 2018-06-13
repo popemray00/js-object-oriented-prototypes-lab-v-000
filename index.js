bbd5b0a08a6fb012bf237758bb47897bb9f327be
Michael Pope <popemray1986@gmail.com>
2018-06-13 21:45:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ BoardMember.prototype.approve = function(){
         return "You will see great things from Scuber."
       }
 
-      BoardMember.prototype.sayHi = function(){
-        return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
-      }
-    }
+  //    BoardMember.prototype.sayHi = function(){
+    //    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+    //  }
+    //}
