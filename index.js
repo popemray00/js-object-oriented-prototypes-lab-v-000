9e7a6d797c796a4e7e2c1b46207dfabda58b387b
Michael Pope <popemray1986@gmail.com>
2018-06-13 21:46:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -47,4 +47,3 @@ BoardMember.prototype.approve = function(){
     BoardMember.prototype.sayHi = function(){
         return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
       }
-    //}
